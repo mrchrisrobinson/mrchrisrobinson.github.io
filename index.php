e017331732b894dc8fe6cb32d11ba2c4c71266fc
mrchrisrobinson <31174634+mrchrisrobinson@users.noreply.github.com>
2017-08-22 21:07:17 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+Written By: Chris Robinson
+First assignment for CS130a
+Modified: 1/18/2017
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Chris's CS130A Introduction to Programming in PHP stuff</title>
+    </head>
+    <body>
+        <h3>Todays date:
+        Wed, 18 Jan 2017 16:11:32 -0800</h3>
+        <p>Links to my assignments are here:</p>
+        <table>
+            <tbody>
+                <tr>
+                    <th>Lab #</th>
+                    <th>Source URL(s)</th>
+                    <th>URL(s)</th>
+                </tr>
+                <tr>
+                    <th>0</th>
+                    <td><a href="/~crobin36/cs130a/index.php?source" target="labs">Lab 1 Source code</a></td>
+                    <td><a href="/~crobin36/cs130a/index.php" target="labs">Lab 1</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
